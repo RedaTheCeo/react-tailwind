1824e005ba19bf5c1d3706a3bce22311d129bc84
Benassila Reda <ridabenassila@gmail.com>
2022-09-03 12:26:33 +0100

    potfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,22 +6,25 @@ const Hero = () => {
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2'>
-          GROWING WITH DATA ANALYTICS
+          Junior Web Developper
         </p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
-          Grow with data.
+          Benassila Reda.
         </h1>
         <div className='flex justify-center items-center'>
           <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-            Fast, flexible financing for
+            Building
           </p>
           <Typed
-          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-            strings={['BTB', 'BTC', 'SASS']}
-            typeSpeed={120}
+            className='md:text-5xl sm:text-4xl text-[#00df9a] text-xl font-bold md:pl-4 pr-1'
+            strings={['Front', 'Back']}
+            typeSpeed={220}
             backSpeed={140}
             loop
           />
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
+            - End Apps
+          </p>
         </div>
         <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
